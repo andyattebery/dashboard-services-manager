19a50484116451aac4c33b5925085812bc208b77
Andy Attebery <me@andyattebery.com>
2026-06-01 00:53:40 -0500

    added nix flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+  outputs = { self, nixpkgs }:
+  let
+    forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
+  in {
+    packages = forAllSystems (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      arch = if system == "aarch64-linux" then "arm64" else "x64";
+      runtimeLibs = with pkgs; [ icu openssl stdenv.cc.cc.lib ];
+    in {
+      dsm-provider = pkgs.stdenv.mkDerivation rec {
+        pname = "dsm-provider";
+        version = "1.1.0";
+        src = pkgs.fetchurl {
+          url = "https://github.com/andyattebery/dashboard-services-manager/releases/download/${version}/dsm-provider-${version}-linux-${arch}.tar.gz";
+          hash = {
+            x64 = "sha256-rtpQaf+OmLOdKX6i0Nb3KKkAw/dddj+VxN2OjTGwARk=";
+            arm64 = "sha256-D+dwAmonQBAN/m3hjJwbY6fuz5VpNltAzGw4oLHTRRM=";
+          }.${arch};
+        };
+        sourceRoot = ".";
+        nativeBuildInputs = with pkgs; [ patchelf makeWrapper ];
+        dontPatchELF = true;
+        dontStrip = true;
+        installPhase = ''
+          install -Dm755 Dsm.Provider.App $out/bin/.dsm-provider-unwrapped
+          patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/.dsm-provider-unwrapped
+          makeWrapper $out/bin/.dsm-provider-unwrapped $out/bin/dsm-provider \
+            --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath runtimeLibs}"
+        '';
+      };
+    });
+  };
+}
